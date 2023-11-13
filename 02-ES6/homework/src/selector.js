d7fe500bb67a1f4e96a513ed7ab7c07263e975b3
RodriguezSN <gamer.infante51@gmail.com>
2023-11-13 05:39:08 -0300

    practica

--- 02-ES6/homework/src/selector.js
+++ 02-ES6/homework/src/selector.js
@@ -1,21 +1,32 @@
 var traverseDomAndCollectElements = function (matchFunc, startEl) {
-  var resultSet = [];
+	var resultSet = [];
 
-  if (typeof startEl === "undefined") {
-    startEl = document.body;
-  }
+	if (typeof startEl === "undefined") {
+		startEl = document.body;
+	}
 
-  // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
-  // usa matchFunc para identificar elementos que matchien
+	// recorre el árbol del DOM y recolecta elementos que matchien en resultSet
+	// usa matchFunc para identificar elementos que matchien
 
-  // TU CÓDIGO AQUÍ
+	// TU CÓDIGO AQUÍ
 };
 
 // Detecta y devuelve el tipo de selector
 // devuelve uno de estos tipos: id, class, tag.class, tag
 
 var selectorTypeMatcher = function (selector) {
-  // tu código aquí
+	// tu código aquí
+	var type = null;
+	if (selector[0] === "#") {
+		type = "id";
+	} else if (selector[0] === ".") {
+		type = "class";
+	} else if (selector[3] === ".") {
+		type = "tag.class";
+	} else {
+		type = "tag";
+	}
+	return type;
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -24,19 +35,22 @@ var selectorTypeMatcher = function (selector) {
 // matchea el selector.
 
 var matchFunctionMaker = function (selector) {
-  var selectorType = selectorTypeMatcher(selector);
-  var matchFunction;
-  if (selectorType === "id") {
-  } else if (selectorType === "class") {
-  } else if (selectorType === "tag.class") {
-  } else if (selectorType === "tag") {
-  }
-  return matchFunction;
+	var selectorType = selectorTypeMatcher(selector);
+	var matchFunction;
+	if (selectorType === "id") {
+	} else if (selectorType === "class") {
+		matchFunction = true;
+	} else if (selectorType === "tag.class") {
+		matchFunction = true;
+	} else if (selectorType === "tag") {
+		matchFunction = true;
+	}
+	return matchFunction;
 };
 
 var $ = function (selector) {
-  var elements;
-  var selectorMatchFunc = matchFunctionMaker(selector);
-  elements = traverseDomAndCollectElements(selectorMatchFunc);
-  return elements;
+	var elements;
+	var selectorMatchFunc = matchFunctionMaker(selector);
+	elements = traverseDomAndCollectElements(selectorMatchFunc);
+	return elements;
 };
